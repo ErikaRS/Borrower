9206e446f925de8dfb961fdd554b31c79678a246
ErikaRS <erika.rice.scherpelz@gmail.com>
2011-06-12 15:19:52 -0700

    Maven setup

---
+++ src/main/java/com/erikars/borrower/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.erikars.borrower;
+
+/**
+ * Placeholder while I learn how to use Maven.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
